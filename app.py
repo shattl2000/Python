4d6d38358aa08fee9ca46006bf3742e5a2e59e9f
Aleksandr Kuzmin <45235717+shattl2000@users.noreply.github.com>
2020-06-08 17:49:48 +0300

    Set var from_lang, to_lang, text

--- app.py
+++ app.py
@@ -3,12 +3,15 @@ import requests
 API_KEY = 'trnsl.1.1.20161025T233221Z.47834a66fd7895d0.a95fd4bfde5c1794fa433453956bd261eae80152'
 URL = 'https://translate.yandex.net/api/v1.5/tr.json/translate'
 
-print("С какого языка переводить")
-from_lang = str(input())
-print("На какой язык переводить")
-to_lang = str(input())
-print("Введите текст для перевода")
-text = str(input())
+# print("С какого языка переводить")
+# from_lang = str(input())
+from_lang = str("en")
+# print("На какой язык переводить")
+# to_lang = str(input())
+to_lang = str("ru")
+# print("Введите текст для перевода")
+# text = str(input())
+text = str("Hello, my friend")
 
 params = {
     'key': API_KEY,
